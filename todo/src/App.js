48a53ba048ab1994925179a1e69561428052fadb
DesmondM <desmondmpofu@gmail.com>
2021-05-22 19:03:15 +0200

    ammended

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,40 +1,128 @@
 import React, {useState} from 'react';
 import './App.css';
-import {DragDropContext} from "react-beautiful-dnd";
-import {lodash}
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import _ from "lodash";
+import {v4} from "uuid";
 
 const item = {
-  id: "123abc",
-  name: "Do job One"
+  id: v4(),
+  name: "Study History"
 }
+
+const item2 = {
+  id: v4(),        // randomly generate an id in string
+  name: "Do some shopping"
+}
+
 function App() {
-  const [state, setState] = useState{initialState{
-    "todo":{
-      title:"Things todo",
-      items: []
+  const [text, setText] = useState("")
+  const [state, setState] = useState({
+    "todo": {
+      title: "TODO",
+      items: [item, item2]
     },
     "in-progress": {
-      title: "Completed",
+      title: "IN PROGRESS",
+      items: []
+    },
+    "done": {
+      title: "COMPLETED",
       items: []
     }
-  }}
+  })
+
+  const handleDragEnd = ({destination, source}) => {
+    if (!destination) {
+      return
+    }
+
+    if (destination.index === source.index && destination.droppableId === source.droppableId) {
+      return
+    }
+
+    // Creating a copy of item before removing it from state
+    const itemCopy = {...state[source.droppableId].items[source.index]}
+
+    setState(prev => {
+      prev = {...prev}
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1)
+
+
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
+
+      return prev
+    })
+  }
+
+  const addItem = () => {
+    setState(prev => {
+      return {
+        ...prev,
+        todo: {
+          title: "TODO",
+          items: [
+            {
+              id: v4(),
+              name: text
+            },
+            ...prev.todo.items
+          ]
+        }
+      }
+    })
+
+    setText("")
+  }
+
   return (
     <div className="App">
-      <DragDropContext onDragEnd={e=>console.log(e)}>
-        {_.map(state, (data, key)=>{
+      <div className="add">
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+        <button className="btn" onClick={addItem}>ADD AN ITEM TO DO</button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd} className="draggable">
+        {_.map(state, (data, key) => {
           return(
-            <Droppable>
-            {(provided=>{
-              return(
-            
-                )
-            }}
-            </Droppable>
+            <div key={key} className={"column"}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return(
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {data.items.map((el, index) => {
+                        return(
+                          <Draggable key={el.id} index={index} draggableId={el.id}>
+                            {(provided, snapshot) => {
+                              console.log(snapshot)
+                              return(
+                                <div
+                                  className={`item ${snapshot.isDragging && "dragging"}`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  )
+                }}
+              </Droppable>
+            </div>
           )
         })}
-      
-      </DragDropContext>    
-       test
+      </DragDropContext>
     </div>
   );
 }
